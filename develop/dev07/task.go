e4028ee1ce3707e372f09ad2486f27f2ef5900e6
Andrey Danko <tor.andrey4312@mail.ru>
2023-12-14 08:15:08 +0300

    Complete tasks 07 and 09

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +39,69 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		// Если не переданы каналы - вернуть nil
+		return nil
+	case 1:
+		// Если передан только один канал - вернуть его
+		return channels[0]
+	}
 
+	var wg sync.WaitGroup
+	orDone := make(chan interface{}, 1) // Буферизованный канал
+
+	// Функция, для получения сигнала о закрытии канала
+	handleChannel := func(ch <-chan interface{}) {
+		defer wg.Done()
+		select {
+		case <-ch:
+			// Если один из каналов закрывается, отправить сигнал в orDone (без блокировки)
+			select {
+			case orDone <- struct{}{}: // Тут используется пустая структура, чтобы подать сигнал без данных и не производить аллокаций
+			default:
+			}
+		case <-orDone:
+		}
+	}
+
+	// Добавляем в группу ожидания количество каналов
+	wg.Add(len(channels))
+
+	// Запускаем для каждого канала функцию
+	for _, ch := range channels {
+		go handleChannel(ch)
+	}
+
+	// Запускаем дополнительную горутину, которая ждет отчёт от каналов и закрывает канал orDone
+	go func() {
+		wg.Wait()
+		close(orDone)
+	}()
+
+	return orDone
+}
+
+func main() {
+	// Согласно тексту задания описываем функцию
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	// Поправили опечатку :)
+	fmt.Printf("Done after %v\n", time.Since(start))
 }
--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +20,89 @@ package main
 */
 
 func main() {
+	// Собираем флаги
+	urlFlag := flag.String("url", "", "URL сайта для скачивания")
+	outputFlag := flag.String("output", "index.html", "Имя файла для сохранения контента")
+	flag.Parse()
 
+	// Проверяем на наличие URL
+	if *urlFlag == "" {
+		fmt.Println("Пожалуйста, укажите URL для скачивания.")
+		os.Exit(1)
+		return
+	}
+
+	// Если нет схемы у URL - добавляем
+	if !strings.HasPrefix(*urlFlag, "http://") && !strings.HasPrefix(*urlFlag, "https://") {
+		*urlFlag = "http://" + *urlFlag
+	}
+
+	// Разбираем URL
+	parsedURL, err := url.Parse(*urlFlag)
+	if err != nil {
+		fmt.Println("Ошибка при разборе URL:", err)
+		os.Exit(1)
+		return
+	}
+
+	// Делаем запрос на сервер и получаем тело ответа
+	content, err := downloadContent(parsedURL)
+	if err != nil {
+		fmt.Println("Ошибка при скачивании контента:", err)
+		os.Exit(1)
+		return
+	}
+
+	// Сохранение контента в файл
+	err = saveToFile(*outputFlag, content)
+	if err != nil {
+		fmt.Println("Ошибка при сохранении в файл:", err)
+		os.Exit(1)
+		return
+	}
+
+	fmt.Println("Скачивание завершено. Контент сохранен в ", *outputFlag)
+}
+
+// downloadContent выполняет запрос на сервер и возвращает тело ответа в виде строки
+func downloadContent(parsedURL *url.URL) (string, error) {
+	response, err := http.Get(parsedURL.String())
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	// Читаем тело ответа
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// saveToFile сохраняет контент страницы в файл
+func saveToFile(filename string, content string) error {
+	// Создаем директорию, если она не существует
+	dir := path.Dir(filename)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Создаем файл
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	// Пишем в файл
+	_, err = io.WriteString(file, content)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
