56080ca87d7dcf5d636f3caa10c27a81e392d8e2
Andrey Danko <tor.andrey4312@mail.ru>
2023-12-14 08:17:53 +0300

    Complete task dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -19,6 +19,50 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
+func sortString(s string) string {
+	// Преобразовываем строку в массив символов
+	sChars := strings.Split(s, "")
+	// Сортируем
+	sort.Strings(sChars)
+	// И соединям символы обратно в строку
+	return strings.Join(sChars, "")
+}
+
+func findAnagrams(words []string) map[string][]string {
+	anagramSets := make(map[string][]string)
+
+	for _, word := range words {
+		// Приводим слова к нижнему регистру и сортируем символы
+		sortedWord := sortString(strings.ToLower(word))
+		// Добавляем слова в множество анаграмм
+		anagramSets[sortedWord] = append(anagramSets[sortedWord], word)
+	}
+
+	// Убираем множества из одного элемента
+	for key, value := range anagramSets {
+		if len(value) < 2 {
+			delete(anagramSets, key)
+		} else {
+			// Сортируем множество по возрастанию
+			sort.Strings(anagramSets[key])
+		}
+	}
+
+	return anagramSets
+}
+
+func main() {
+	words := []string{"пятак", "пятка", "пол", "тяпка", "листок", "слиток", "столик", "кот", "ток", "кто", "сам", "стул"}
+	fmt.Printf("Исходный срез: %s\n", words)
+	anagramSets := findAnagrams(words)
+
+	for _, value := range anagramSets {
+		fmt.Printf("Множество анаграмм для %s: %v\n", value[0], value)
+	}
 }
